4c44b57087e87a0729b76d7a8a6e4c9195b6860e
Andrew Langmeier <raymi306@gmail.com>
2022-10-29 18:50:55 -0400

    Fix up movement, add collision
    Movement now has inertia!

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,11 @@ use std::time::Duration;
 use engine::{
     Context, Engine, GameState, run
 };
-use engine::types::{Color, Rect, Vec2, Vec2F, VirtualKeyCode};
+use engine::types::{Color, Rect, Vec2, Vec2F, VirtualKeyCode, WinitInputHelper};
 use engine::drawing::draw_rectangle;
 
 const SCREEN_WIDTH: u32 = 320;
 const SCREEN_HEIGHT: u32 = 240;
-
-struct Game {
-    ctx: Context,
-    level: Level,
-    camera: Camera,
-    player: Player,
-}
 
 struct Level {
     width: u32,
@@ -27,22 +20,22 @@ impl Default for Level {
         let width = 64;
         let height = 16;
         let mut repr = "".to_owned();
-        repr += "................................................................";
-        repr += "................................................................";
-        repr += "................................................................";
-        repr += "................................................................";
-        repr += "................................................................";
-        repr += "................................................................";
-        repr += "........................#.......................................";
-        repr += ".......................##.......................................";
-        repr += ".........#............###.###.##...............................#";
-        repr += ".....................####.......................................";
-        repr += "....................#####.......................................";
-        repr += "###########################.#######.############################";
-        repr += "..........................#.#.....#.#...........................";
-        repr += "..........................#.#######.#...........................";
-        repr += "..........................#.........#...........................";
-        repr += "..........................###########...........................";
+        repr += "################################################################";
+        repr += "#...............................................................";
+        repr += "#.....................................############........#.....";
+        repr += "#................................................#........#.....";
+        repr += "#................................................#........#.....";
+        repr += "#................................................#........#.....";
+        repr += "#.......................#.......................................";
+        repr += "#......................##.......................................";
+        repr += "#........#............###.###.##...............................#";
+        repr += "#....................####.......................................";
+        repr += "#...................#####.......................................";
+        repr += "####.######################.#######.#.##########################";
+        repr += "#.........................#.#.....#.#...........................";
+        repr += "#.........................#.###.###.#...........................";
+        repr += "#.........................#.........#...........................";
+        repr += "#.........................###########...........................";
         Self { width, height, repr }
     }
 }
@@ -66,15 +59,23 @@ struct Player {
 impl Default for Player {
     fn default() -> Self {
         Self {
-            pos: Vec2F::new(0.0, 0.0),
+            pos: Vec2F::new(1.0, 1.0),
             vel: Vec2F::new(0.0, 0.0),
-            speed: 6.0,
+            speed: 7.0,
         }
     }
 }
 
 const TILE_WIDTH: u32 = 16;
 const TILE_HEIGHT: u32 = 16;
+
+struct Game {
+    ctx: Context,
+    level: Level,
+    camera: Camera,
+    player: Player,
+    movement_keys: [VirtualKeyCode; 4],
+}
 
 impl Game {
     pub fn new() -> Self {
@@ -86,8 +87,18 @@ impl Game {
         let level = Level::default();
         let camera = Camera::default();
         let player = Player::default();
-        Self { ctx, level, camera, player }
+        let movement_keys = [VirtualKeyCode::Up, VirtualKeyCode::Down, VirtualKeyCode::Left, VirtualKeyCode::Right] ;
+        Self { ctx, level, camera, player, movement_keys }
     }
+}
+
+fn any_key_held(input: &WinitInputHelper, keys: &[VirtualKeyCode]) -> bool {
+    for key in keys {
+        if input.key_held(*key) {
+            return true;
+        }
+    }
+    false
 }
 
 impl Game {
@@ -110,39 +121,68 @@ impl GameState for Game {
 
         screen.clear(Color::new(50, 50, 193, 255));
 
+        let mut direction = Vec2F::new(0.0, 0.0);
         if engine.input.key_held(VirtualKeyCode::Up) {
-            self.player.vel.y = -1.0;
+            direction.y = -1.0;
         }
         if engine.input.key_held(VirtualKeyCode::Down) {
-            self.player.vel.y = 1.0;
+            direction.y = 1.0;
         }
         if engine.input.key_held(VirtualKeyCode::Left) {
-            self.player.vel.x = -1.0;
+            direction.x = -1.0;
         }
         if engine.input.key_held(VirtualKeyCode::Right) {
-            self.player.vel.x = 1.0;
+            direction.x = 1.0;
         }
 
-        println!("{:?}", self.player.vel);
-        self.player.vel = self.player.vel.normalize() * self.player.speed;
-
-        /*
-        self.player.vel.x += -4.0 * self.player.vel.x * elapsed_time.as_secs_f32();
-        if f32::abs(self.player.vel.x) < 0.13 {
-            self.player.vel.x = 0.0;
+        if direction.magnitude() > 1.0 {
+            direction = direction.normalize();
         }
-        self.player.vel.y += -4.0 * self.player.vel.y * elapsed_time.as_secs_f32();
-        if f32::abs(self.player.vel.y) < 0.13 {
-            self.player.vel.y = 0.0;
-        }
-        */
-        println!("{:?}", self.player.vel);
 
-        self.player.pos.x += self.player.vel.x * elapsed_time.as_secs_f32();
-        self.player.pos.y += self.player.vel.y * elapsed_time.as_secs_f32();
+        let target_velocity = direction * self.player.speed * elapsed_time.as_secs_f32();
+        let friction = 0.049;
+        self.player.vel.x = self.player.vel.x + (target_velocity.x - self.player.vel.x) * friction;
+        self.player.vel.y = self.player.vel.y + (target_velocity.y - self.player.vel.y) * friction;
+
+        let mut new_position = Vec2F::new(0.0, 0.0);
+        new_position.x = self.player.pos.x + self.player.vel.x;
+        new_position.y = self.player.pos.y + self.player.vel.y;
+
+        // Collision handling
+        if self.player.vel.x <= 0.0 { // moving left
+            if self.get_tile(new_position.x as i32, self.player.pos.y as i32) != '.' ||
+                self.get_tile(new_position.x as i32, (self.player.pos.y + 0.9) as i32) != '.' {
+                    new_position.x = new_position.x.trunc() + 1.0;
+                    self.player.vel.x = 0.0;
+                    println!("Collision x left");
+            }
+        } else { // moving right
+            if self.get_tile((new_position.x + 1.0) as i32, self.player.pos.y as i32) != '.' ||
+                self.get_tile((new_position.x + 1.0) as i32, (self.player.pos.y + 0.9) as i32) != '.' {
+                    new_position.x = new_position.x.trunc();
+                    self.player.vel.x = 0.0;
+                    println!("Collision x right");
+            }
+        }
+        if self.player.vel.y <= 0.0 { // moving up
+            if self.get_tile(new_position.x as i32, new_position.y as i32) != '.' ||
+                self.get_tile((new_position.x + 0.9) as i32, new_position.y as i32) != '.' {
+                    new_position.y = new_position.y.trunc() + 1.0;
+                    self.player.vel.y = 0.0;
+                    println!("Collision y up");
+            }
+        } else { // moving down
+            if self.get_tile(new_position.x as i32, (new_position.y + 1.0) as i32) != '.' ||
+                self.get_tile((new_position.x + 0.9) as i32, (new_position.y + 1.0) as i32) != '.' {
+                    new_position.y = new_position.y.trunc();
+                    self.player.vel.y = 0.0;
+                    println!("Collision y down");
+            }
+        }
+
         
-        self.camera.pos.x = self.player.pos.x;
-        self.camera.pos.y = self.player.pos.y;
+        self.player.pos = new_position;
+        self.camera.pos = self.player.pos;
 
         let visible_tiles_x = SCREEN_WIDTH / TILE_WIDTH;
         let visible_tiles_y = SCREEN_HEIGHT / TILE_HEIGHT;
